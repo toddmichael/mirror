94ee584ed2de6270825fd8a0bae39ee3165ae54b
Jesse Nelson <spheromak@gmail.com>
2012-08-16 13:22:53 +0900

    bump version for CPS-106 work

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,7 @@ maintainer_email "spheromak@gmail.com"
 license          "Apache 2.0"
 description      "Set up various Mirrors"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
+version          "1.0.1"
 
 depends "apache2"
+depends "rsync"
