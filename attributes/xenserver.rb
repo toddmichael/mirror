1d33431b67d12726625fce9c1fe7c5039449f446
Jesse Nelson <spheromak@gmail.com>
2012-08-17 14:53:02 +0900

    fix typo in authoritative

--- attributes/xenserver.rb
+++ attributes/xenserver.rb
@@ -2,6 +2,6 @@
 default[:mirror][:xenserver][:enabled] = true
 default[:mirror][:xenserver][:url] = "rsync://10.4.1.159/xenserver-prod"
 default[:mirror][:xenserver][:bw_limit] = 0
-default[:mirror][:xenserver][:authritative] = true
+default[:mirror][:xenserver][:authoritative] = true
 default[:mirror][:xenserver][:exclude] = []
 
