dc26920a13289aedc8a020367006bfa004e36aa2
Jesse Nelson <spheromak@gmail.com>
2012-08-21 17:04:28 +0900

    update epel excludes

--- attributes/epel.rb
+++ attributes/epel.rb
@@ -4,27 +4,17 @@ default[:mirror][:epel][:enabled] = true
 default[:mirror][:epel][:url] = "rsync://dl.fedoraproject.org/fedora-epel"
 default[:mirror][:epel][:bw_limit] = 5000
 default[:mirror][:epel][:exclude] =  %w{
-*/ppc64/
-/2/
-/2.*
-/3/
-/3.*
-/4/
-/4.*
-apt/
-docs/
-isos/
-SRPMS/
-alpha/
-ia64/
-ppc/
-s390/
-s390x/
-Deployment_Guide-*
-5server
-5Server
-/5.5
-/5.6
-/5.7
+  beta/
+  testing/
+  beta testing 
+  6/ppc64/ 
+  6/SRPMS/
+  5/ppc
+  5/ppc64
+  SRPMS
+  4*
+  */SRPMS/*
+  */ppc/*
+  */ppc64/*
 }
 
