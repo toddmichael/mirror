850622a238bd3c42971c9653afef854f667d5908
Jesse Nelson <spheromak@gmail.com>
2012-09-13 15:48:29 -0700

    add ubuntu 12.04 (precise)

---
+++ attributes/ubuntu.rb
@@ -0,0 +1,6 @@
+default[:mirror][:ubuntu_precise][:enabled] = true
+default[:mirror][:ubuntu_precise][:url] = "rsync://ftp.riken.jp/ubuntu/dists/precise/"
+default[:mirror][:ubuntu_precise][:bw_limit] = 10000
+default[:mirror][:ubuntu_precise][:exclude] = []
+
+
