e9a4c7a0aa77d93aa2b835d0fe4eb728c2a84149
Jesse Nelson <spheromak@gmail.com>
2013-03-19 05:02:07 -0700

    add name to metadata

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,10 @@
+name             "mirror"
 maintainer       "Jesse Nelson"
 maintainer_email "spheromak@gmail.com"
 license          "Apache 2.0"
 description      "Set up various Mirrors"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.2"
+version          "1.0.3"
 
 depends "apache2"
 depends "rsync", ">= 0.7.2"
